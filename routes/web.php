c63b9f7650a6b077f67579f13ba840169ab8352d
3A532089 <likebestupid@gmail.com>
2018-12-31 07:31:39 +0800

    基礎路由設置

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('student/{student_no}',function($student_no){
+    return "學號:".$student_no;
+});
+Route:get('student/{student_no}/score',function($student_no){
+    return "學號:".$student_no."的所有成績";
+});
