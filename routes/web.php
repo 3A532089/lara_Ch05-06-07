27939e0d425407c91c043528b00ab1a27e1c083d
3A532089 <likebestupid@gmail.com>
2018-12-31 08:24:10 +0800

    第六個練習

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,24 @@ Route::get('/', function () {
 });
 
 Route::pattern('student_no','s[0-9]{10}');
-Route::get('student/{student_no}',function($student_no){
+/*Route::get('student/{student_no}',function($student_no){
     return "學號:".$student_no;
 });
 
-/*Route::get('student/{student_no}/score',function($student_no){
+Route::get('student/{student_no}/score',function($student_no){
     return "學號:".$student_no."的所有成績";
-});*/
+});
 
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
     return "學號:".$student_no."的".((is_null($subject)))."成績";
-})->(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
+})->(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);*/
+
+Route::group(['prefix'=>'student'],function(){
+    Route::get('{student_no}',function($student_no){
+        return "學號:".$student_no;
+    });
+    Route::get('{student_no}/score/{subject?}',function($student_no,$subject = null){
+        return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+    })->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
+});
+    
