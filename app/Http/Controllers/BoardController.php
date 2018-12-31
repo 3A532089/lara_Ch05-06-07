de18dc93a0fd94048edbd63c3f48b595c803814d
3A532089 <likebestupid@gmail.com>
2018-12-31 10:03:14 +0800

    score.blade建立

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -7,5 +7,5 @@ use Illuminate\Http\Request;
 class BoardController extends Controller
 {
     public function getIndex(){
-        return view('board');
+        return view::make('board');
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 Route::get('/','HomeController@index');
 Route::get('cool','Cool\TestController@index');
-Route::get('board','TestController@getIndex');
+
+Route::resource('board','BoardController@getIndex');
 /*Route::pattern('student_no','s[0-9]{10}');
 Route::get('student/{student_no}',function($student_no){
     return "學號:".$student_no;
@@ -26,7 +27,7 @@ Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject
     return "學號:".$student_no."的".((is_null($subject)))."成績";
 })->(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);*/
 
-/*Route::group(['prefix'=>'student'],function(){
+Route::group(['prefix'=>'student'],function(){
     Route::get('{student_no}',[
         'as'=>'student',
         'uses'=>'StudentController@getStudentData'
@@ -36,5 +37,5 @@ Route::get('{student_no}/score/{subject?}',[
     'as'=>'student.score',
     'uses'=>'StudentController@getStudentScore'
 ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
-});*/
+});
     
