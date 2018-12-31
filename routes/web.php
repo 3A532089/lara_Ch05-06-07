51bff09bfde4eeb814d0155ecd0390a277b414fd
3A532089 <likebestupid@gmail.com>
2018-12-31 08:53:28 +0800

    第十次練習

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 Route::get('/','HomeController@index');
+Route::get('cool','Cool\TestController@index');
 /*Route::pattern('student_no','s[0-9]{10}');
 Route::get('student/{student_no}',function($student_no){
     return "學號:".$student_no;
@@ -24,7 +25,7 @@ Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject
     return "學號:".$student_no."的".((is_null($subject)))."成績";
 })->(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);*/
 
-Route::group(['prefix'=>'student'],function(){
+/*Route::group(['prefix'=>'student'],function(){
     Route::get('{student_no}',[
         'as'=>'student',
         'uses'=>'StudentController@getStudentData'
@@ -34,5 +35,5 @@ Route::get('{student_no}/score/{subject?}',[
     'as'=>'student.score',
     'uses'=>'StudentController@getStudentScore'
 ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
-});
+});*/
     
