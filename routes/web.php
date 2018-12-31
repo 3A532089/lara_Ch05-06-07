a21abe97fc12e5c7390fc561d38e670340b7dda9
3A532089 <likebestupid@gmail.com>
2018-12-31 08:31:56 +0800

    第八個練習

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/','HomeController@index')
 Route::pattern('student_no','s[0-9]{10}');
-/*Route::get('student/{student_no}',function($student_no){
+Route::get('student/{student_no}',function($student_no){
     return "學號:".$student_no;
 });
 
@@ -26,9 +22,9 @@ Route::get('student/{student_no}/score',function($student_no){
 
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
     return "學號:".$student_no."的".((is_null($subject)))."成績";
-})->(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);*/
+})->(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
 
-Route::group(['prefix'=>'student'],function(){
+/*Route::group(['prefix'=>'student'],function(){
     Route::get('{student_no}',[
         'as'=>'student',
         'uses'=>function($student_no){
@@ -41,5 +37,5 @@ Route::get('{student_no}/score/{subject?}',[
         return '學號:'.$student_no.'  的'.((is_null($subject))?'所有科目':$subject).'成績';
     }
 ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
-});
+});*/
     
